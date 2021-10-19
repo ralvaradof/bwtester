460c6eea801ace4b3365626e1f6c3d70bcf0538e
ralvarado <ralvaradof@me.com>
2021-10-19 11:45:05 -0300

    Modificacion del proceso de kill para iperf3

--- bwtest.fo.ftth.bash
+++ bwtest.fo.ftth.bash
@@ -59,7 +59,7 @@ sudo ./cbandwidth-gtd -config=config.tester.yml &
 chromium-browser http://127.0.0.1/dashboards --password-store=basic --no-default-browser-check
 #firefox http://127.0.0.1/dashboards --password-store=basic
 pgrep cbandwidth | xargs sudo kill -9
-pgrep iperf3 | xargs sudo kill -9
+pgrep iperf3 | xargs sudo kill
 echo  "########################################################################"
 echo  "Ejecucion finalizada"
 echo  "########################################################################"
